1e54f939fa514fb5dcb30418aa6ee390273219eb
Andi Leach <me@duck.me.uk>
2026-09-15 12:16:23 +0100

    Add VITE_APP_NAME customisation envvar
    Signed-off-by: Andi Leach <me@duck.me.uk>

--- frontend/src/components/AppLogo.vue
+++ frontend/src/components/AppLogo.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
+const appName = import.meta.env.VITE_APP_NAME ? import.meta.env.VITE_APP_NAME : "Slurp"
 </script>
 <template>
   <div class="flex flex-nowrap items-center">
     <UIcon class="flex-1 text-2xl" name="simple-icons:cbs"/>
-    <span class="flex-1 pl-2 font-bold text-2xl">Slurp</span>
+    <span class="flex-1 pl-2 font-bold text-2xl">{{ appName }}</span>
   </div>
-
 </template>
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+interface ViteTypeOptions {
+  // Disallow unknown keys.
+  strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
