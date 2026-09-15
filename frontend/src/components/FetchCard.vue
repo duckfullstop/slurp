ae032cb07ea4fdd74fddc62fa03cf2fc06008815
Andi Leach <me@duck.me.uk>
2026-09-15 19:26:57 +0100

    Janitorial work / styling
    Signed-off-by: Andi Leach <me@duck.me.uk>

--- frontend/src/components/FetchCard.vue
+++ frontend/src/components/FetchCard.vue
@@ -20,63 +20,85 @@ let highlightColor = computed(() => {
       return 'neutral'
   }
 })
+
+let taskSlugLeading = computed(() => {
+  return props.task.slug.split(' ', 1)[0]
+})
+
+let taskSlugTrailing = computed(() => {
+  // fun and interesting way of getting everything after the first space
+  return props.task.slug.split(' ').slice(1).join(' ')
+})
+
 </script>
 <template>
-  <UPageCard
-    :highlight-color="highlightColor"
-    highlight
-  >
-    <template #title>
-      <div class="space-x-1 flex items-center">
-        <UBadge
-          class="text-1xl"
-          color="neutral"
-        >
-          <UIcon name="material-symbols:snail"/>
-          {{ task.slug }}
-        </UBadge>
-        <UBadge
-          :color="highlightColor"
-          class="text-1xl"
-        >
-          {{ task.status }}
-        </UBadge>
-      </div>
-    </template>
-    <template #description>
-      <span>{{ task.url }}</span>
-    </template>
-    <template #footer>
-      <div class="space-x-1 flex items-center justify-center">
-        <UTooltip
-          :delay-duration="0"
-          text="Slurp Job ID"
-        >
+  <UChip :color="highlightColor" :text="task.status" :ui="{base: 'p-2 pl-3 ring-0 rounded-t-none rounded-r-none'}" inset
+         size="3xl">
+    <UPageCard
+      :highlight-color="highlightColor"
+      highlight
+    >
+      <template #title>
+        <div class="space-x-1 flex items-center">
           <UBadge
+            class="text-1xl"
+            color="neutral"
+          >
+            <UIcon name="material-symbols:snail"/>
+            <span
+              class="font-bold"
+            >
+              {{ taskSlugLeading }}
+            </span>
+          </UBadge>
+          <UBadge
+            v-if="taskSlugTrailing"
+            class="text-1xl"
+            color="neutral"
+          >
+            <UIcon name="material-symbols:description"/>
+            <span
+            >
+              {{ taskSlugTrailing }}
+            </span>
+          </UBadge>
+        </div>
+      </template>
+      <template #description>
+        <p class="wrap-anywhere">{{ task.url }}</p>
+      </template>
+      <template #footer>
+        <div class="space-x-1 flex items-center justify-center">
+          <UTooltip
+            :delay-duration="0"
+            text="Slurp Job ID"
+          >
+            <UBadge
+              color="neutral"
+              variant="outline"
+            >
+              <UIcon name="pepicons-pop:soft-drink-circle"/>
+              <span
+                v-if="props.extended"
+                class="text-muted"
+              >
+              {{ task.id.slice(0, -4) }}
+            </span>
+              <span class="font-bold">
+              {{ task.id.slice(-4) }}
+            </span>
+            </UBadge>
+          </UTooltip>
+          <UBadge
+            v-if="task.ts_created"
             color="neutral"
             variant="outline"
           >
-            <UIcon name="pepicons-pop:soft-drink-circle"/>
-            <span
-              v-if="props.extended"
-              class="text-muted"
-            >
-              {{ task.id.slice(0, -4) }}
-            </span>
-            <span class="font-bold">
-              {{ task.id.slice(-4) }}
-            </span>
+            <UIcon name="pepicons-pop:clock"/>
+            {{ format(task.ts_created, 'yyyy-MM-dd HH:MM') }} UTC
           </UBadge>
-        </UTooltip>
-        <UBadge
-          v-if="task.ts_created"
-          color="neutral"
-          variant="outline"
-        >
-          <UIcon name="pepicons-pop:clock"/>
-          {{ format(task.ts_created, 'yyyy-MM-dd HH:MM') }} UTC
-        </UBadge>
-      </div>
-    </template>
-  </UPageCard>
+        </div>
+      </template>
+    </UPageCard>
+  </UChip>
 </template>
